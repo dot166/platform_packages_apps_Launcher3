74ede7f30dc20afda72afc62883d6d925e770bdb
._______166 <retromac9x2@gmail.com>
2026-05-09 10:39:52 +0100

    add nexus -1
    includes "Revert "Revert^2 "Removing some deprecated APIs around LauncherOverlay"""
    
    This reverts commit a9c8f119812e49903dbc04b54e211c112777454d.

---
+++ src/com/google/android/launcherclient/Constant.java
@@ -0,0 +1,14 @@
+package com.google.android.launcherclient;
+
+/**
+ * Created by yuchuan
+ * DATE 2020/4/17
+ * TIME 14:58
+ */
+public final class Constant {
+
+    public static final String GSA_PACKAGE = "io.github.dot166.nexus";
+    public static final String ACTION = "com.android.launcher3.WINDOW_OVERLAY";
+
+
+}
