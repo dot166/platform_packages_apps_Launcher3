2378a14d478fc2fdd13d88f1a2eef54226c8efcd
._______166 <retromac9x2@gmail.com>
2026-05-09 10:39:52 +0100

    add nexus -1
    includes "Revert "Revert^2 "Removing some deprecated APIs around LauncherOverlay"""
    
    This reverts commit a9c8f119812e49903dbc04b54e211c112777454d.

--- quickstep/src/com/android/quickstep/LauncherActivityInterface.java
+++ quickstep/src/com/android/quickstep/LauncherActivityInterface.java
@@ -38,6 +38,7 @@ import com.android.app.displaylib.PerDisplayRepository;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherAnimUtils;
+import com.android.launcher3.LauncherInitListener;
 import com.android.launcher3.LauncherState;
 import com.android.launcher3.anim.PendingAnimation;
 import com.android.launcher3.dagger.LauncherAppSingleton;
@@ -47,15 +48,14 @@ import com.android.launcher3.statemanager.StateManager;
 import com.android.launcher3.taskbar.TaskbarInteractor;
 import com.android.launcher3.uioverrides.QuickstepLauncher;
 import com.android.launcher3.util.DaggerSingletonObject;
+import com.android.launcher3.util.ThreadedAnimator;
 import com.android.launcher3.util.JoinedAnimator;
 import com.android.launcher3.util.NavigationMode;
-import com.android.launcher3.util.ThreadedAnimator;
 import com.android.launcher3.views.ScrimColors;
 import com.android.quickstep.GestureState.GestureEndTarget;
 import com.android.quickstep.dagger.QuickstepBaseAppComponent;
 import com.android.quickstep.orientation.RecentsPagedOrientationHandler;
 import com.android.quickstep.util.AnimatorControllerWithResistance;
-import com.android.quickstep.util.ContextInitListener;
 import com.android.quickstep.util.LayoutUtils;
 import com.android.quickstep.views.RecentsView;
 import com.android.systemui.plugins.shared.LauncherOverlayManager;
@@ -158,11 +158,9 @@ public final class LauncherActivityInterface extends
     }
 
     @Override
-    public ContextInitListener<Launcher> createActivityInitListener(
-            Predicate<Boolean> onInitListener) {
-        return new ContextInitListener<>(
-                (activity, alreadyOnHome) -> onInitListener.test(alreadyOnHome),
-                Launcher.ACTIVITY_TRACKER);
+    public LauncherInitListener createActivityInitListener(Predicate<Boolean> onInitListener) {
+        return new LauncherInitListener((activity, alreadyOnHome) ->
+                onInitListener.test(alreadyOnHome));
     }
 
     @Override
